be38b14f38c9450e5986d89b0da4a7f10a7f61f5
Egill <egill@egill.xyz>
2020-11-05 12:00:06 +0000

    no message

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,14 @@
 set -e
 
 rm -f build/index.html
-rm -f build/css.css
+# rm -f build/css.css
 
 # npm run build
 
+npm run styles
+
 cp website/frontend/index.html build/index.html
-cp website/frontend/css.css build/css.css
+# cp website/frontend/css.css build/css.css
 
 # JS_HASH=($(sha1sum build/js/website.js))
 # CSS_HASH=($(sha1sum build/css.css))
@@ -15,3 +17,4 @@ JS_HASH=($(shasum build/js/website.js))
 CSS_HASH=($(shasum build/css.css))
 sed -i -E "s/website\.js\?build=[[:alnum:]]*/website.js?build=${JS_HASH:0:8}/" build/index.html
 sed -i -E "s/css\.css\?build=[[:alnum:]]*/css.css?build=${CSS_HASH:0:8}/" build/index.html
+rm -f build/index.html-E
